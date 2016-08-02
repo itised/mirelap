c2ebd7a7ef67019b76158b64e488a4a18807ee2f
Edward DeMaio <ed@itsed.com>
2016-08-02 14:52:47 -0400

    added provider to publish config

---
+++ src/Providers/MirelapServiceProvider.php
@@ -0,0 +1,22 @@
+<?php namespace Mirelap\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class MirelapServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '../../config/mirelap.php' => config_path('mirelap.php')
+        ]);
+    }
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+}
